dd4526818d934c8ab577252670ab3ba793029913
Matt Woolford <57449285+mattwoolford@users.noreply.github.com>
2025-11-05 14:55:23 +0000

    Initial commit - Implement wait for GPS fix - Implement arm and disarm functionality - Remove `MavLink_messages.py`

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from MavLink_messages import *
+import time
+
 from pymavlink import mavutil
+
+from actions import *
+
+mavlink = mavutil.mavlink
 
 # Connect to MAVLink
 protocol = "TCP"
 host = "127.0.0.1"
-port = 5760
+port = 14550
 protocol = protocol.lower()
 connection_str = ":".join([protocol, host, str(port)])
-print(connection_str)
 print(f"Connecting to MAVLink at {protocol}://{host}:{port}...")
 conn = mavutil.mavlink_connection(connection_str)
 conn.wait_heartbeat()
 print(f"Connected to MAVLink at {protocol}://{host}:{port}")
-
-while True:
-    msg = conn.recv_msg()
-    if not msg:
-        continue
-    if msg.get_type() == HEARTBEAT:
-        print(f"HEARTBEAT: {msg}")
+wait_for_GPS_fix(conn)
+arm(conn)
+time.sleep(10)
+disarm(conn)
